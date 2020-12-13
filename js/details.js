e709a4a58983b2a39d65f8d551ba345891e870ed
musa_abdullah <musaabdalwahed@gmail.com>
2020-12-13 16:49:05 +0200

    delete

--- js/details.js
+++ js/details.js
@@ -1,4 +1,25 @@
-const id = new URLSearchParams('window.location.search').get('id')
+const id = new URLSearchParams(window.location.search).get('id')
+
+const post = document.querySelector('.card-body');
 
 
-console.log(id);
+const postDetail = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    const postData = await res.json();
+
+    template = `
+    <h5>${postData.title}</h5>
+    <p>${postData.body}</p>
+    `;
+    console.log(id);
+    console.log(postData);
+    post.innerHTML = template;
+}
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", () => postDetail());
